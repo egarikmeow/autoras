cb1f7a6eecffd89188970c7ce8b3ef36ed178118
Егор <egorlysikov2@gmail.com>
2025-06-22 17:37:13 +0700

    Пуш

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ from aiogram.fsm.context import FSMContext
 from telethon import TelegramClient
 from telethon.errors.rpcerrorlist import ChatWriteForbiddenError, SessionPasswordNeededError
 
-API_TOKEN = '7762245807:AAH_4SIIHqqDKugyvkTZzQUyMBoMC8P5c_4'
-TESTER_ID = 6060082547
+API_TOKEN = '7762245807:AAF5DdRUXGUISjuQJdPRMjOHYjaUQp-O5d8'
+TESTER_ID = 8188636934
 TELEGRAM_API_ID = 24144091
 TELEGRAM_API_HASH = '35f8ffb23ce7378da704a39810962c61'
 
